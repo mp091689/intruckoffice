986c33115f7dfb51cc4243783f7273cfc1bc3882
Mykyta <mr.mykyta.popov@gmail.com>
2023-09-12 01:36:23 -0600

    checking

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ ini_set ('display_errors', 1);
 ini_set ('display_startup_errors', 1);
 error_reporting (E_ALL);
 
+phpinfo();
+die;
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
