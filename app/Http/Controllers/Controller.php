df5485570c730085d9e948cfe217b2f5acc9d12f
Mykyta <mr.mykyta.popov@gmail.com>
2023-09-20 12:26:33 -0600

    check environment

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Str;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return view('welcome', [
+            'DB_CONNECTION' => env('DB_CONNECTION'),
+            'DB_HOST' => env('DB_HOST'),
+            'DB_PORT' => env('DB_PORT'),
+            'DB_USERNAME' => env('DB_USERNAME'),
+            'DB_PASSWORD' => env('DB_PASSWORD'),
+        ]);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,15 @@
                     <h1 class="ml-4 mt-6 text-xl font-semibold text-gray-900 dark:text-white">Hello World 4</h1>
                 </div>
 
+                <ul class="dark:text-white">
+                    <li>DB_CONNECTION {{ $DB_CONNECTION }}</li>
+                    <li>DB_HOST {{ Str::limit($DB_HOST, 5) }}</li>
+                    <li>DB_PORT {{ $DB_PORT }}</li>
+                    <li>DB_USERNAME {{ $DB_USERNAME }}</li>
+                    <li>DB_PASSWORD {{ Str::limit($DB_PASSWORD, 5) }}</li>
+                    <li>testenv {{ $testenv }}</li>
+                </ul>
+
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
