9a2cdd62d46999db2b6a175e5ce7bb9731b2e6ca
Mykyta <mr.mykyta.popov@gmail.com>
2023-09-20 21:40:59 -0600

    work with env

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Config;
 
 class Controller extends BaseController
 {
@@ -13,12 +12,6 @@ class Controller extends BaseController
 
     public function index()
     {
-        return view('welcome', [
-            'DB_CONNECTION' => Config::get('DB_CONNECTION'),
-            'DB_HOST' => Config::get('DB_HOST'),
-            'DB_PORT' => Config::get('DB_PORT'),
-            'DB_USERNAME' => Config::get('DB_USERNAME'),
-            'DB_PASSWORD' => Config::get('DB_PASSWORD'),
-        ]);
+        echo var_dump($_ENV);
     }
 }
