0bbb066593502f6565c7458538ef4cc99f3ba6e9
Mykyta <mr.mykyta.popov@gmail.com>
2023-09-24 01:03:40 -0500

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,3 +2,5 @@
 
 cd /app
 php artisan migrate
+
+/usr/local/bin/mxnet-model-server --start --foreground --mms-config /home/model-server/config.properties --models densenet=https://dlc-samples.s3.amazonaws.com/pytorch/multi-model-server/densenet/densenet.mar
