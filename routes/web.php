5c30051426ae140cc64fb26e98f815402e4d254a
Mykyta <mr.mykyta.popov@gmail.com>
2023-09-24 23:06:26 -0500

    clean up welcome

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', function () {
-        return view('welcome');
-    })->name('home');
-
-    Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
