f86284a5c62fcabdb9949f9b8b4c135e2a476ea4
Mykyta <mr.mykyta.popov@gmail.com>
2023-11-10 20:00:48 -0600

    fix vite js buld

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/charts.js',
                 'resources/js/invoice.js',
+                'resources/js/work.js',
             ],
             refresh: true,
         }),
