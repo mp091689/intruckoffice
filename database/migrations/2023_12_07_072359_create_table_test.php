d34e5aa57c6b7f635fdaf3c41e60ef2630a60e17
Mykyta <mr.mykyta.popov@gmail.com>
2023-12-07 02:00:28 -0700

    test 5

--- database/migrations/2023_12_07_072359_create_table_test.php
+++ database/migrations/2023_12_07_072359_create_table_test.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_test_4', function (Blueprint $table) {
+        Schema::create('table_test_5', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_test_4');
+        Schema::dropIfExists('table_test_5');
     }
 };
